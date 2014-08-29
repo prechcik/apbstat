6616cf4e81c48f6e7bc0f88a98e7898fd006719c
asser-dk <asser-dk@outlook.com>
2014-08-29 14:40:23 +0200

    added isrunning variable to handle when to start and stop the log scanner

--- ApbStat/Utilities/LogScanner.cs
+++ ApbStat/Utilities/LogScanner.cs
@@ -7,8 +7,11 @@
         public event KillsAssistsStunsOrArrestsEventHandler OnNewKillsAssistsStunsOrArrests;
 
         public event LogRestartedEventHandler OnLogRestarted;
+
+        private bool IsRunning { get; set; }
         public void Dispose()
         {
+            EndLogScanning();
         }
         }
     }
